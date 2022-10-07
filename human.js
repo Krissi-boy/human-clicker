63211377ff4b80cb37ffbdf8ca8b21957eda84a1
Kristoffer <kristoffer.mn@hotmail.no>
2022-10-07 14:18:26 +0200

    Added VAR
    Prisen i butikken oppdaterer seg etter hvor mange ganger jeg trykker på produktet

--- human.js
+++ human.js
@@ -8,12 +8,15 @@ var adoptions_price_EL = document.querySelector("#adoptions_price")
 var cloning_price_EL = document.querySelector("#cloning_price")
 
 
-human_amount = 0;
+var human_amount = 0;
 click = 0;
 adoptions_amount = 0;
 steal_amount = 0;
 cloning_amount = 0;
 
+var steal_price = 50;
+adoptions_price = 500;
+cloning_price = 1500;
 
 
 function human_click() {
@@ -22,32 +25,40 @@ function human_click() {
     human_num_EL.innerHTML = human_amount 
 }
 function adoptions_click() {
-    if
-
-    console.log(adoptions_EL)
+    if (human_amount > adoptions_price)
+    {
+    human_amount -= adoptions_price
+    adoptions_price * 2
     adoptions_amount += 2
-    human_num_EL.innerHTML = adoptions_amount
-    adoptions_price_EL.innerHTML = adoptions_price
+    adoptions_price_EL.innerHTML = "Price:" + adoptions_price
+    }
 }
 function steal_click() {
-    if (human_amount > steal_price )
+    if (human_amount >= steal_price)
     {
-
-    console.log(steal_EL)
-    steal_amount += 100
-    human_num_EL.innerHTML = steal_amount
-    innerHTML = steal_price_EL
+    human_amount -= steal_price
+    steal_price *= 50
+    steal_amount += 1
+    steal_price_EL.innerHTML = "Price: " + steal_price
+    }
 }
-function cloning_click() {
-    if
 
-    console.log(cloning_EL)
+function cloning_click() {
+    if (human_amount > cloning_price)
+    {
+    human_amount -= cloning_price
+    cloning_price *= 5
     cloning_amount += 1000
-    human_num_EL.innerHTML = cloning_amount
-    innerHTML = cloning_price_EL
+    cloning_price_EL.innerHTML = "Price:" + cloning_price
+    }
 }
 
 human_EL.addEventListener("click", human_click)
 adoptions_EL.addEventListener("click", adoptions_click)
 steal_EL.addEventListener("click", steal_click)
 cloning_EL.addEventListener("click", cloning_click)
+
+var timer = setInterval(myTimer, 10); // kjører funksjonen myTimer 1 gang i sekundet
+function myTimer(){ 
+    human_num_EL.innerHTML = human_amount;
+}
